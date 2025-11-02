99e80005a948858929dfd9a71e373ff1fc4affec
kiliankoeck <kilian@koeck@hotmail.com>
2025-11-02 11:33:48 +0100

    improved page header regex and removed presidium list if present

--- extract_text.py
+++ extract_text.py
@@ -1,5 +1,6 @@
 import re
 import nltk
+nltk.download('punkt_tab')
 from nltk.tokenize import sent_tokenize
 # start with "Beginn der Sitzung: x{x}.xx Uhr"
 # end with "Schluss der Sitzung: x{x}.xx Uhr"
@@ -14,13 +15,20 @@ pattern = r"Beginn der Sitzung: \d{1,2}[.]\d{2} Uhr(.*?)Schluss der Sitzung: \d{
 match = re.search(pattern,document,re.DOTALL)
 protocol = match.group(1).strip() if match else document
 
+# Remove list of presidium if present
+if protocol.startswith("Vorsitzende"):
+    m = re.search(r"^Vorsitzende[r]?:.*?\*{5}\s*(.*)$", protocol, re.DOTALL)
+    protocol = m.group(1).strip() if m else protocol
+
 # remove additional remarks in parentheses (mostly referring to applause) 
 # this also removes which parties the speakers are from (could be done at a later time?)
 # and things shouted inbetween by other people
 protocol = re.sub(r"\([^()]*\)","",protocol)
 
 # remove the new page headers e.g. (Bundesrat Stenographisches Protokoll 851. Sitzung / Seite 10)
-protocol = re.sub(r"Bundesrat Stenographisches Protokoll(.*?)Seite \d*","",protocol,re.DOTALL)
+PAGE_HEADER_REGEX = re.compile(r'((Bundesrat\s)?Stenographisches\sProtokoll\s\d+\.\sSitzung\s\/\sSeite\s\d+)')
+protocol = re.sub(PAGE_HEADER_REGEX,'',protocol)
+# protocol = re.sub(r"Bundesrat Stenographisches Protokoll(.*?)Seite \d*","",protocol,re.DOTALL)
 protocol = protocol.replace("\xad","") # remove soft hyphens
 
 
