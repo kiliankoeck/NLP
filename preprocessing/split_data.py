7a77ca0a8454ccd1957456924154afd89be2fbce
millas1 <krandisk@gmail.com>
2025-11-23 11:16:36 +0100

    Completed split_data script and updated run_preprocessing, README, requirements.txt, and .gitignore

--- preprocessing/split_data.py
+++ preprocessing/split_data.py
@@ -9,10 +9,10 @@ output_dir = './data/splits'
 
 train_dir = os.path.join(output_dir,'train')
 test_dir = os.path.join(output_dir,'test')
-val_dir = os.path.join(output_dir,'val')
+val_dir = os.path.join(output_dir,'validation')
 
-train_ratio = 0.7
-test_val_ratio = 0.5
+train_ratio = 0.7       #70% training
+test_val_ratio = 0.5    #15% testing & 15% validation
 
 random_seed = 42
 
@@ -38,4 +38,22 @@ def split_and_copy():
 
     print("\nStarting file copy")
 
-    
+    def copy_set(file_list, new_dir, name):
+
+        print(f"\nCopying {len(file_list)} files to {name} set ({new_dir}):")
+
+        for filename in file_list:
+            source_path = os.path.join(input_dir, filename)
+            new_path = os.path.join(new_dir, filename)
+            shutil.copy2(source_path,new_path)
+
+        print(f"Finished copying {len(file_list)} files for {name}")
+
+    copy_set(train_files, train_dir, "Training")
+    copy_set(test_files, test_dir, "Testing")
+    copy_set(val_files, val_dir, "Validation")
+
+    print("\n Data splitting and copying completed")
+
+if __name__ == "__main__":
+    split_and_copy()
